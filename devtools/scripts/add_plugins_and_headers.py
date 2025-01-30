50863a243c18421479203437c355530bc1029331
Evan Pretti <pretti@stanford.edu>
2025-01-30 12:36:28 -0800

    More SO patching

--- devtools/scripts/add_plugins_and_headers.py
+++ devtools/scripts/add_plugins_and_headers.py
@@ -43,3 +43,9 @@ for filename in os.listdir('.'):
                     print(f'patching {soname!r}: rpath {rpath} -> {new_rpath}')
                     subprocess.run(['patchelf', '--remove-rpath', soname], check=True)
                     subprocess.run(['patchelf', '--force-rpath', '--set-rpath', ':'.join(new_rpath), soname], check=True)
+            for soname in os.listdir('OpenMM.libs/lib/plugins'):
+                # These need an rpath to look in the parent directory for
+                # libOpenMMTorch.so; it doesn't get loaded by _openmm.*.so with
+                # libOpenMM.so and for some reason it can't then be found by the
+                # plugins...
+                subprocess.run(['patchelf', '--force-rpath', '--set-rpath', '$ORIGIN/..', os.path.join('OpenMM.libs/lib/plugins', soname)])
